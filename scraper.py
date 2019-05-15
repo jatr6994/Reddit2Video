2ab6edc7d180609c3020a4edf869e77deb45cd4f
jatr6994 <jatr6994@colorado.edu>
2019-05-15 12:33:06 -0600

    got data for pipeline

--- scraper.py
+++ scraper.py
@@ -1,83 +1,54 @@
-from os.path import isfile
 import praw
-import pandas as pd
-from time import sleep
 import sys
 
 
 class SubredditScraper:
-	def __init__(self, sub, sort='new', lim=900, mode='w'):
+	def __init__(self, sub, sort='new', time='all', lim=100):
 		self.sub = sub
-		self.sort = sort #new top
+		self.sort = sort
+		self.time = time
 		self.lim = lim
-		self.mode = mode
-		print( f'SubredditScraper instance created with values '
-				f'sub = {sub}, sort = {sort}, lim = {lim}, mode = {mode}')
+
 
 	def set_sort(self):
 		if self.sort == 'new':
-			return self.sort, reddit.subreddit(self.sub).new(limit=self.lim)
+			return self.sort, reddit.subreddit(self.sub).new(time_filter=self.time, limit=self.lim)
 		elif self.sort == 'top':
-			return self.sort, reddit.subreddit(self.sub).top(limit=self.lim)
+			return self.sort, reddit.subreddit(self.sub).top(time_filter=self.time, limit=self.lim)
 		elif self.sort == 'hot':
-			return self.sort, reddit.subreddit(self.sub).hot(limit=self.lim)
+			return self.sort, reddit.subreddit(self.sub).hot(time_filter=self.time, limit=self.lim)
 		else:
 			self.sort = 'hot'
-			print('Sort method was not recognized, defaulting to hot.')
-			return self.sort, reddit.subreddit(self.sub).hot(limit=self.lim)
+			return self.sort, reddit.subreddit(self.sub).hot(time_filter=self.time, limit=self.lim)
 
 	def get_posts(self):
-		"""Get unique posts from a specified subreddit."""
-
-		sub_dict = { 'selftext': [], 'title': [], 'id': [], 'sorted_by': [],
-					'num_comments': [], 'score': [], 'ups': [], 'downs': []}
-		
-		csv = f'{self.sub}_posts.csv'
-
-		# Attempt to specify a sorting method.
+		post = {}
 		sort, subreddit = self.set_sort()
 
-		# Set csv_loaded to True if csv exists since you can't 
-		# evaluate the truth value of a DataFrame.
-		df, csv_loaded = (pd.read_csv(csv), 1) if isfile(csv) else ('', 0)
+		for p in subreddit:
+			post['selftext'] = p.selftext
+			post['title'] = p.title
+			post['id'] = p.id
 
-		print(f'csv = {csv}')
-		print(f'After set_sort(), sort = {sort} and sub = {self.sub}')
-		print(f'csv_loaded = {csv_loaded}')
+		print(post)
 
-		print(f'Collecting information from r/{self.sub}.')
+		return post
 
 
-		for post in subreddit:
-			# Check if post.id is in df and set to True if df is empty.
-			# This way new posts are still added to dictionary when df = ''
-			unique_id = post.id not in tuple(df.id) if csv_loaded else True
+	def get_comments(self, post):
+		submission = reddit.submission(id=post['id'])
+		submission.comment_sort = 'best'
+		submission.comment_limit = 3
+		res = {}
 
-			# Save any unique posts to sub_dict.
-			if unique_id:
-				sub_dict['selftext'].append(post.selftext)
-				sub_dict['title'].append(post.title)
-				sub_dict['id'].append(post.id)
-				sub_dict['sorted_by'].append(sort)
-				sub_dict['num_comments'].append(post.num_comments)
-				sub_dict['score'].append(post.score)
-				sub_dict['ups'].append(post.ups)
-				sub_dict['downs'].append(post.downs)
-			sleep(0.1)
+		for comment in submission.comments:
+			try:
+				res[comment.author] = comment.body
+			except:
+				pass
 
-			new_df = pd.DataFrame(sub_dict)
 
-			# Add new_df to df if df exists then save it to a csv.
-			if 'DataFrame' in str(type(df)) and self.mode == 'w':
-				pd.concat([df, new_df], axis=0, sort=0).to_csv(csv, index=False)
-				print(f'{len(new_df)} new posts collected and added to {csv}')
-			elif self.mode == 'w':
-				new_df.to_csv(csv, index=False)
-				print(f'{len(new_df)} posts collected and saved to {csv}')
-			else:
-				print(f'{len(new_df)} posts were collected but they were not '
-					f'added to {csv} because mode was set to "{self.mode}"')
-
+		return res
 
 if __name__ == "__main__":
 	reddit = praw.Reddit(client_id = sys.argv[1],
@@ -86,4 +57,10 @@ if __name__ == "__main__":
 					password = sys.argv[4],
 					user_agent = sys.argv[5])
 
-	SubredditScraper(sys.argv[6], lim=997, mode='w', sort='top').get_posts()
+	data = SubredditScraper(sys.argv[6], sort='top', time='week', lim=1)
+	post = data.get_posts()
+	important = data.get_comments(post)
+	for k, v in important.items():
+		print(k)
+		print(v)
+		print('---------------------------------')
