9f440291e2b4f1cfaef33ff80570d92154db0cf0
Michael Rahn <m7ur7l3@yahoo.com>
2019-05-17 16:47:05 -0600

    better animation
    gonna abort puppeteer too probably and will just figure out a way to directly record smoothly idk

--- puppeteer/pup.js
+++ puppeteer/pup.js
@@ -8,13 +8,13 @@ function delay(time) {
 }
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch( { defaultViewport: { width: 1200, height: 600 }, headless: false } );
   const page = await browser.newPage();
 
-  await page.tracing.start({ screenshots: true, path: 'trace.json' });
-  // await page.goto('file:///C:/Users/7UR7L3/Documents/MEGAsync/dev/projects/reddit2video/Reddit2Video/cssAnim.html', { timeout: 60000 });
-  await page.goto('file:///C:/Users/Jacob/Desktop/Reddit2Video/cssAnim.html', { timeout: 60000 });
-  await delay( 10000 );
+  await page.tracing.start({ screenshots: true, path: 'trace.json' /*, categories: [ "disabled-by-default-devtools.screenshot" ]*/ });
+  await page.goto('file:///C:/Users/7UR7L3/Documents/MEGAsync/dev/projects/reddit2video/Reddit2Video/cssAnim.html', { timeout: 60000 });
+  // await page.goto('file:///C:/Users/Jacob/Desktop/Reddit2Video/cssAnim.html', { timeout: 60000 });
+  await delay( 20000 );
   await page.tracing.stop();
 
   // --- extracting data from trace.json ---
@@ -25,6 +25,7 @@ function delay(time) {
       typeof x.args !== 'undefined' &&
       typeof x.args.snapshot !== 'undefined'
   ));
+  console.log( traceScreenshots.length )
   traceScreenshots.forEach(function(snap, index) {
     fs.writeFile('frames/trace-screenshot-'+("00000"+index).substr(-5)+'.png', snap.args.snapshot, 'base64', function(err) {
       if (err) {
